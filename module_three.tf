94648ec677589dc42bdfb0573fa1a4ca2c10035f
tekhie <49533456+tekhie@users.noreply.github.com>
2020-09-16 11:39:34 +0100

    Update module_three.tf

--- module_three.tf
+++ module_three.tf
@@ -72,7 +72,7 @@ resource "aws_security_group" "allow_ssh" {
 resource "aws_instance" "nginx" {
   ami                    = data.aws_ami.aws-linux.id
   instance_type          = "t2.micro"
-  key_name               = var.key_name
+#  key_name               = var.key_name
   vpc_security_group_ids = [aws_security_group.allow_ssh.id]
 
   connection {

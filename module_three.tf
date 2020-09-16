1a0a8ec28b2dfa5b38ce267c83cb0d2e22cc0c2c
tekhie <49533456+tekhie@users.noreply.github.com>
2020-09-16 11:11:53 +0100

    Update module_three.tf

--- module_three.tf
+++ module_three.tf
@@ -4,8 +4,8 @@
 
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
-variable "private_key_path" {}
-variable "key_name" {}
+#variable "private_key_path" {}
+#variable "key_name" {}
 variable "region" {
   default = "us-east-1"
 }

8f1887bf3a72b90eda9a0af0113804bc894b29dc
tekhie <49533456+tekhie@users.noreply.github.com>
2020-09-16 11:15:16 +0100

    Update module_three.tf

--- module_three.tf
+++ module_three.tf
@@ -89,7 +89,7 @@ resource "aws_instance" "nginx" {
     type        = "ssh"
     host        = self.public_ip
     user        = "ec2-user"
-    private_key = file(var.private_key_path)
+#    private_key = file(var.private_key_path)
 
   }
 
